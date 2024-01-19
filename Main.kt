1f62cbb11e8b52eba4c0caceec33e94f6e697988
fufukoku <fufukokunof@gmail.com>
2024-01-19 10:41:27 +0900

    Change the main to make it clean

--- Main.kt
+++ Main.kt
@@ -1,24 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-//    val dog0: Animal = Dog("Hachiko")
-//    dog0.move("Forward")
-//    dog0.makeASound()
-//
-//
-//    val cat0: Cat = Cat("Tom")
-//    cat0.move("Forward")
-//    cat0.makeASound()
-//    println("\n")
-//
-//    moveAndMakeASound(dog0)
-//    moveAndMakeASound(cat0)
     val mouse0: Mouse =  Mouse("Jerry")
-    mouse0.makeASound();
-    println(mouse0.CheeseAmount)
-    mouse0.StealCheese(1)
-    mouse0.StealCheese(1)
-    mouse0.EatCheese()
+    mouse0.makeASound()
 }
 
 fun moveAndMakeASound(animal: Animal){ //Polymorphism
