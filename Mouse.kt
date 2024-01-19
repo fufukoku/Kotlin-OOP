a2fa704d1e3904ba9aa3799d486bfc4c06cf20ad
fufukoku <fufukokunof@gmail.com>
2024-01-19 10:27:04 +0900

    Create Mouse.kt

---
+++ Mouse.kt
@@ -0,0 +1,23 @@
+class Mouse(_name:String = "Jerry"): Animal(_name){
+    override fun makeASound(){
+        squeaks();
+    }
+
+    private fun squeaks(){
+        println("$animalName said squeaks")
+    }
+
+    private var CheeseAmount: Int = 0
+
+    private fun StealCheese(x: Int){
+        println("before"+CheeseAmount)
+        CheeseAmount+=x
+        println("after"+CheeseAmount)
+    }
+
+    private fun EatCheese(){
+        println("before"+CheeseAmount)
+        CheeseAmount--
+        println("after"+CheeseAmount)
+    }
+}
